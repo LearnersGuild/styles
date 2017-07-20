d94020e1ce1d834074e9c1e628e7c2d2f1f73264
Tanner Welsh <tannerwelsh@gmail.com>
2017-07-20 18:14:27 -0400

    v0.4.1: Use absolute path for font URLs in dist
    Refactor build script
    Move fonts to subdirectory assets/

--- scripts/build.js
+++ scripts/build.js
@@ -3,37 +3,44 @@ const path = require('path')
 const sass = require('node-sass')
 
 const ROOT_PATH = path.resolve(__dirname, '..')
+const SRC_PATH = path.resolve(ROOT_PATH, 'src')
+const DIST_PATH = path.resolve(ROOT_PATH, 'dist')
+const SOURCE_NAME = 'base'
+const DIST_FONT_PATH = '/assets/fonts/'
 
-const config = {
-  sources: [
-    'base',
-  ],
-  outputStyles: [
-    'nested',
-    'compressed',
-  ],
-}
+const inputSCSS = `
+$fonts-path: "${DIST_FONT_PATH}"; // override default for dist
 
-const outFileName = (source, outputStyle) => {
+@import "${SOURCE_NAME}";
+`
+
+const outputStyles = [
+  'nested',
+  'compressed',
+]
+
+const outFileName = (outputStyle) => {
   return outputStyle === 'compressed' ?
-    `${source}.min.css` :
-    `${source}.css`
+    `${SOURCE_NAME}.min.css` :
+    `${SOURCE_NAME}.css`
 }
 
-config.sources.forEach((source) => {
-  config.outputStyles.forEach((outputStyle) => {
-    const file = path.resolve(ROOT_PATH, 'src', `${source}.scss`)
-    const outFile = path.resolve(ROOT_PATH, 'dist', outFileName(source, outputStyle))
+outputStyles.forEach((outputStyle) => {
+  const outFile = path.resolve(DIST_PATH, outFileName(outputStyle))
 
-    const options = { file, outFile, outputStyle }
+  const options = {
+    data: inputSCSS,
+    outFile,
+    outputStyle,
+    includePaths: [SRC_PATH]
+  }
 
-    sass.render(options, (error, result) => {
+  sass.render(options, (error, result) => {
+    if (error) return console.error(error)
+
+    fs.writeFile(outFile, result.css, function(err) {
       if (error) return console.error(error)
-
-      fs.writeFile(outFile, result.css, function(err) {
-        if (error) return console.error(error)
-        console.log(`Compiled ${file} => ${outFile}`)
-      })
+      console.log(`Compiled ${SOURCE_NAME}.scss => ${outFile}`)
     })
   })
 })
